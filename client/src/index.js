4162ca3372cfcb1e64144a38f01f885eacd96c7e
Lukasz Szmit <lukasz@szmit.eu>
2022-06-16 23:43:14 +0100

    Fix import order

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,7 @@
 import 'core-js/stable';
 import 'regenerator-runtime';
-import App from 'modules/app/components/App';
-
 import React from 'react';
 import { render } from 'react-dom';
+import App from 'modules/app/components/App';
 
 render(<App />, document.getElementById('root'));
